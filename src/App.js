27b35a6ac0105500ed0949a3fe89183143d63568
MuhammadRabius <muhammadrabiussany@gmail.com>
2022-04-02 23:04:20 +0600

    react router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import { Routes, Route } from "react-router-dom";
+import Order from './components/Order/Order';
+import Inventoery from './components/Inventoey/Inventoery';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      
+      <Routes>
+        
+        <Route path='/' element={<Shop></Shop>}></Route>
+        <Route path='/shop'  element={<Shop></Shop>}></Route>
+        <Route path='/orders'  element={<Order></Order>}></Route>
+        <Route path='/inventory' element={<Inventoery></Inventoery>}></Route>
+        <Route path='/*' element={<NotFound/>}></Route>
+        
+
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Inventoey/Inventoery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventoery = () => {
+      return (
+            <div>
+                  <h1>This is Inventory Section</h1>
+            </div>
+      );
+};
+
+export default Inventoery;
